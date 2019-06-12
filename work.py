5f72e032f3cc25d9888c5bf0558a26342cfa722f
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2019-06-12 13:22:18 +0530

    Changed code according to dynamic page number URL

--- work.py
+++ work.py
@@ -2,24 +2,35 @@ import requests
 from lxml import html
 from helper import download
 
-url = "https://arxiv.org/search/advanced?advanced=1&terms-0-operator=AND&terms-0-term=&terms-0-field=title&classification-computer_science=y&classification-physics_archives=all&classification-include_cross_list=include&date-filter_by=all_dates&date-year=&date-from_date=&date-to_date=&date-date_type=announced_date_first&abstracts=hide&size=50&order=-announced_date_first"
+for i in range(2):
 
-response = requests.get(url)
+    url = "https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=&terms-0-field=title&classification-computer_science=y&classification-physics_archives=all&classification-include_cross_list=include&date-filter_by=all_dates&date-year=&date-from_date=&date-to_date=&date-date_type=announced_date_first&abstracts=hide&size=50&order=-announced_date_first&start={page_num}"
+    url = url.format(page_num=i*50)
 
-tree = html.fromstring(response.content)
+    response = requests.get(url)
 
-filenames = tree.xpath('/html/body/main/div[2]/ol/li[position()<=3]/div/p/a')
-elements = tree.xpath('/html/body/main/div[2]/ol/li[position()<=3]/div/p/span/a[1]')
+    tree = html.fromstring(response.content)
 
-for filename, element in zip(filenames, elements):
-    pdf_file_name = filename.text
-    pdf_file_name = pdf_file_name.replace(':', '_')
-    pdf_file_name = pdf_file_name.replace('.', '_')
-    pdf_file_name += ".pdf"
+    range_of_files_per_page = "position()<=2"
 
-    pdf_file_link = element.attrib['href']
-    pdf_file_link += ".pdf"
+    filename_xpath = '/html/body/main/div[2]/ol/li[{range_files}]/div/p/a'
+    element_xpath = '/html/body/main/div[2]/ol/li[{range_files}]/div/p/span/a[1]'
 
-    download(pdf_file_link, pdf_file_name)
+    filename_xpath = filename_xpath.format(range_files=range_of_files_per_page)
+    element_xpath = element_xpath.format(range_files=range_of_files_per_page)
+
+    filenames = tree.xpath(filename_xpath)
+    elements = tree.xpath(element_xpath)
+
+    for filename, element in zip(filenames, elements):
+        pdf_file_name = filename.text
+        pdf_file_name = pdf_file_name.replace(':', '_')
+        pdf_file_name = pdf_file_name.replace('.', '_')
+        pdf_file_name += ".pdf"
+
+        pdf_file_link = element.attrib['href']
+        pdf_file_link += ".pdf"
+
+        download(pdf_file_link, pdf_file_name)
 
 
